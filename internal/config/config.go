b209c6443257581f2e0c212116761354d5c41d02
aphiwe-citiq <aphiwem@citiqprepaid.co.za>
2025-11-17 15:52:44 +0200

    WIP

---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+type Config struct {
+	Database struct {
+		Host     string `mapstructure:"host"`
+		Port     int    `mapstructure:"port"`
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Name     string `mapstructure:"name"`
+	} `mapstructure:"database"`
+
+	Logging struct {
+		Level string `mapstructure:"level"`
+	} `mapstructure:"logging"`
+
+	Scripts struct {
+		BackupPath string `mapstructure:"backup_path`
+	} `mapstricture:"scripts"`
+}
+
+var C Config
---
+++ internal/scripts/root.go
@@ -0,0 +1,35 @@
+package scripts
+
+import (
+	"github.com/Aphiwe-Makisi/one_binary/internal/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "goscripts",
+	Short: "A CLI exposing multiple Go scripts in a single binary",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return initConfig()
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yml", "config file (yaml format)")
+}
+
+func initConfig() error {
+	viper.SetConfigFile(cfgFile)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	if err := viper.Unmarshal(&config.C); err != nil {
+		return err
+	}
+
+	return nil
+}
